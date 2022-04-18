c1ae7b1952b7f0b2d033d3a9bba7c21e25cd0efd
DeAndre Boston <dboston3@yahoo.com>
2022-04-17 19:26:32 -0500

    Callback paractice

--- callback.js
+++ callback.js
@@ -1,12 +1,42 @@
-// asynchronous
-setTimeout(() => {
-    console.log('five seconds are up')
-}, 5000)
 
-// synchronous
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
-})
+// setTimeout(() => {
+//     console.log('five seconds are up')
+// }, 5000)
 
-console.log(shortNames)
+
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
+
+// const geocode = (address, callback) => {
+//     setTimeout(()=>{
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//         callback(data)
+//     }, 2000)
+    
+// }
+
+// geocode('Philidelphia', (data)=>{
+//     console.log(data)
+// })
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+const add = (num1, num2, callback) => {
+    setTimeout(()=>{
+        const sum  = num1 + num2
+        callback(sum)
+    }, 3000)
+}
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+});
